884a92df5a4105b34f4d8d8f1a9b22579e5d8758
salmon2 <dys04076@naver.com>
2021-10-18 13:54:40 +0900

    fisrt commit

---
+++ src/main/java/com/sparta/NeflixCloneCodingProjectBack/NeflixCloneCodingProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.NeflixCloneCodingProjectBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeflixCloneCodingProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeflixCloneCodingProjectBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/NeflixCloneCodingProjectBack/NeflixCloneCodingProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.NeflixCloneCodingProjectBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeflixCloneCodingProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
