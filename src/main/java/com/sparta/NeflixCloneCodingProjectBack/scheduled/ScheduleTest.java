616e142aada06b06bd0a38e60da44be201b4bee4
salmon2 <dys04076@naver.com>
2021-10-19 11:41:36 +0900

    scheduler test

--- src/main/java/com/sparta/NeflixCloneCodingProjectBack/scheduled/ScheduleTest.java
+++ src/main/java/com/sparta/NeflixCloneCodingProjectBack/scheduled/ScheduleTest.java
@@ -1,9 +1,11 @@
 package com.sparta.NeflixCloneCodingProjectBack.scheduled;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 
+//@Component
 public class ScheduleTest {
     @Scheduled(fixedDelay = 1000) // scheduler 끝나는 시간 기준으로 1000 간격으로 실행
     public void scheduleFixedDelayTask() {
@@ -11,16 +13,4 @@ public class ScheduleTest {
                 "Fixed delay task - " + System.currentTimeMillis() / 1000);
     }
 
-    @Scheduled(fixedRate = 1000) // scheduler 시작하는 시간 기준으로 1000 간격으로 실행
-    public void scheduleFixedRateTask() {
-        System.out.println(
-                "Fixed rate task - " + System.currentTimeMillis() / 1000);
-    }
-
-    @Scheduled(cron = "0 15 10 15 * ?") // cron에 따라 실행
-    public void scheduleTaskUsingCronExpression() {
-        long now = System.currentTimeMillis() / 1000;
-        System.out.println(
-                "schedule tasks using cron jobs - " + now);
-    }
 }
