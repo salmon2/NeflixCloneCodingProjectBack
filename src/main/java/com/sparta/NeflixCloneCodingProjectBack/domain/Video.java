c20d2fff84f9612cf1f4baa6d9eaa460cb320546
salmon2 <dys04076@naver.com>
2021-10-19 15:36:53 +0900

    Schedule Vedio Insert complete

--- src/main/java/com/sparta/NeflixCloneCodingProjectBack/domain/Video.java
+++ src/main/java/com/sparta/NeflixCloneCodingProjectBack/domain/Video.java
@@ -1,5 +1,7 @@
 package com.sparta.NeflixCloneCodingProjectBack.domain;
 
+import com.sparta.NeflixCloneCodingProjectBack.dto.themovieapibygenredto.TheMovieApiResponseResultList;
+import com.sparta.NeflixCloneCodingProjectBack.dto.themovieapibyiddto.VideoListResult;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +15,19 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class Video {
+    public Video(TheMovieApiResponseResultList movie, VideoListResult youtube) {
+        this.adult = movie.getAdult();
+        this.title = movie.getTitle();
+        this.original_language = movie.getOriginal_language();
+        this.original_title = movie.getOriginal_title();
+        this.overview = movie.getOverview();
+        this.popularity = movie.getPopularity();
+        this.posterPath = "https://image.tmdb.org/t/p/w500"+movie.getPoster_path();
+        this.release_date = movie.getRelease_date();
+        this.youtubePath = "https://www.youtube.com/embed/"+youtube.getKey()+"?autoplay=1&mute=1";
+        this.vote_average = movie.getVote_average();
+        this.vote_count = movie.getVote_count();
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,8 +36,7 @@ public class Video {
     @Column
     private Boolean adult;
 
-    @Column
-    private String backdrop_path;
+
 
     @Column(nullable = false)
     private String title;
@@ -33,20 +47,20 @@ public class Video {
     @Column
     private String original_title;
 
-    @Column
+    @Column( length = 100000 )
     private String overview;
 
     @Column
     private Float popularity;
 
     @Column
-    private String poster_path;
+    private String posterPath;
 
     @Column
     private String release_date;
 
     @Column
-    private Boolean video;
+    private String youtubePath;
 
     @Column
     private Float vote_average;
